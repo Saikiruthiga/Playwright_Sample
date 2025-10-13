ad8a4b8289654f5d486a843a967d5168e4767212
Saikiruthiga <saikiruthiga06@gmail.com>
2025-10-13 11:48:21 +0200

    Remove the before each since there is 1 test

--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-});
 test('verify initial page rendering', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
   await expect(page).toHaveTitle(/Rating/);
   const ratingCircles = page.getByTestId('rating-circles');
   const circles = ratingCircles.locator('> div');
